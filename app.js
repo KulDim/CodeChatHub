ee201c235ea2c276f0a0b879ddafda8896f41808
Вадим <61011733+KulDim@users.noreply.github.com>
2023-11-14 21:02:32 +0300

    ну такое

--- app.js
+++ app.js
@@ -1,30 +1,47 @@
+// Подключение модулей
 const express = require('express');
-const app = express();
 const http = require('http');
+const socketIO = require('socket.io');
+
+// Создание приложения Express
+const app = express();
 const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+const io = socketIO(server);
 
-const connectedSockets = [];
+// Поддержка статических файлов (например, HTML, CSS)
+app.use(express.static(__dirname + '/public'));
 
+// Отправка файла index.html при обращении к корневому URL
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
+// Событие при подключении нового пользователя
 io.on('connection', (socket) => {
-    socket.id = 0
-    connectedSockets.push({id: 0, name: 'name'})
-
-    io.emit("connection",'chat message');
-
-    console.log(connectedSockets);
-});
-
-io.on('disconnect', () => {
-    console.log('Пользователь отключен');
-    delete connectedSockets[socket.id];
+    console.log('Пользователь подключен');
+  
+    // Генерация уникального имени пользователя
+    const username = 'Пользователь' + Math.floor(Math.random() * 1000);
+  
+    // Отправка сообщения о подключении нового пользователя с его именем
+    io.emit('chat message', { type: 'system', text: `${username} присоединился к чату` });
+  
+    // Событие при отправке сообщения
+    socket.on('chat message', (msg) => {
+      console.log('Сообщение: ' + msg);
+      // Отправка сообщения всем подключенным пользователям с именем отправителя
+      io.emit('chat message', { type: 'user', username: username, text: msg });
+    });
+  
+    // Событие при отключении пользователя
+    socket.on('disconnect', () => {
+      console.log('Пользователь отключен');
+      // Отправка сообщения о отключении пользователя с его именем
+      io.emit('chat message', { type: 'system', text: `${username} покинул чат` });
+    });
   });
 
+// Запуск сервера на порту 3000
 server.listen(3000, () => {
-    console.log('listening on *:3000');
+  console.log('Сервер запущен на порту 3000');
 });
