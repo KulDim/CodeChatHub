6fcff5c0174b95ff48cba4f7b5eb0824961356aa
Вадим <61011733+KulDim@users.noreply.github.com>
2023-11-20 21:23:33 +0300

    add jsonwebtoken

--- client/src/components/home/home.jsx
+++ client/src/components/home/home.jsx
@@ -1,23 +1,47 @@
+import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import styles from "./styles.module.css"
-import { useState } from "react"
+import axios from 'axios';
 
-function Home() {
-    const [user, setUser] = useState('');
+function Home({socket}) {
+    const [formData, setFormData] = useState({
+        username: '',
+        password: '',
+        socketIO: '',
+    })
+
+
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
     const navigate = useNavigate()
 
     const onSubmitSend = (e) => {
         e.preventDefault()
-        localStorage.setItem('user', user)
         navigate('/CodeChatHub')
+    }
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData((prevData) => ({
+          ...prevData,
+          [name]: value,
+        }))
+      }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        formData.socketIO = socket.id
+        const response = await axios.post(`${window.location.origin}/api/auth`, formData);
+        console.log(response.data);
+        // localStorage.setItem('auth', jws_token)
     }
 
     return (
         <div className={styles.Home}>
             <h1 className={styles.title}>CodeChatHub</h1>
-            <form onSubmit={onSubmitSend} className={styles.form}>
-                <input className={styles.input} type="text" onChange={(e) => setUser(e.target.value)} value={user}/>
-                <button className={styles.button}>войти</button>
+            <form onSubmit={handleSubmit} className={styles.form}>
+                <input className={styles.input} type="text" onChange={handleChange} value={formData.username} placeholder="username" name="username"/>
+                <input className={styles.input} type="text" onChange={handleChange} value={formData.password} placeholder="password" name="password"/>
+                <button className={styles.button}>Войти</button>
             </form>
         </div>
     )
--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const app = express()
 const http = require('http').Server(app)
 const cors = require('cors')
 const path = require('path')
+const jwt = require('jsonwebtoken')
 
 const socketIO = require('socket.io')(http, {
     cors: {
@@ -11,17 +12,26 @@ const socketIO = require('socket.io')(http, {
 })
 
 const PORT = 3000
+const tokenKey = '1a2b-3c4d-5e6f-7g8h';
 
 
+let db = {}
 
-app.get('/api', (req, res) => {
-    res.json({
-        message: 'hello'
-    })
+app.use(express.json());
+
+app.post('/api/auth', (req, res) => {
+    const json = req.body
+    // jwt.verify('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZ…M4OX0.vPlw3yoZBLxp2vtVMO05uICjjtKm8-ErVqsIhbgcdaI', tokenKey, (e,d) => {
+    //     console.log(d)
+    // })
+    return res.status(200).json({token: jwt.sign(req.body, tokenKey)})
 })
 
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
+app.use((req, res, next) => {
+    return res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'))
+})
 
 socketIO.on('connection', (socket)  => {
     console.log('connection')
@@ -32,12 +42,11 @@ socketIO.on('connection', (socket)  => {
         socketIO.emit('response', data)
     })
 
-    socketIO.on('disconnect', () => {
+    socket.on('disconnect', () => {
         console.log('disconnect')
         console.log(socket.id)
     })
 })
-
 
 http.listen(PORT, () => {
     console.log('server starting')
